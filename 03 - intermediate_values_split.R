a215b155809ce0ef0049010a3691332e30d732d5
aoliveram <anibal.olivera.m@gmail.com>
2025-04-07 18:47:50 -0400

    reos con solo 1 delito

--- 03 - intermediate_values_split.R
+++ 03 - intermediate_values_split.R
@@ -8,7 +8,6 @@
 #   4.4 Delitos económicos
 #   4.5 Violencia intrafamiliar
 # 5) Seleccionaremos a los reos con casos puros: sin imputaciones de más de 1 delito.
-#    Esto se hace en el script 'datos reos - exploracion.R'
 
 library(dplyr)
 library(tidyr)
@@ -20,6 +19,53 @@ library(qgraph)
 library(igraph)
 library(doParallel)
 
+# ------------------------------------------------------------------------------
+# Creación objeto para 03 - intermediate_values_split.R
+# ------------------------------------------------------------------------------
+
+base_igi_bin <- read.csv("psy_net_files/base_igi_bin_1.csv")
+reinc_por_reo <- readRDS("documentos carceles - ising/Datos reos - sensible/reinc_por_reo.rds")
+
+length(unique(base_igi_bin$COD_PERS)) # 17276
+length(unique(reinc_por_reo$COD_PERS)) # 97123
+length(datos_reos_reinc$COD_PERS) # 203130 --> hay reos que han reincidido varias veces 
+
+# Como hay reos que han reincidido varias veces, hay que tomar una decisión: 
+#   1) tratamos cada reincidencia como un caso nuevo ('nuevo reo'), o
+#   2) tratamos cada reo como único, y consideramos solo su primera reincidencia
+# Acá adoptamos la opción 1).
+
+# Seleccionamos "COD_PERS" , "SEXO", "COMUNA_DOMICILIO", "FECHA_NACIMIENTO", "DELITOS"
+reos_filtrados <- datos_reos_reinc %>%
+  select(COD_PERS, SEXO, COMUNA_DOMICILIO, FECHA_NACIMIENTO, DELITOS) #203130
+
+# Filtramos las columnas que tienen delitos compuestos: más de un delito (#171755) 
+reos_filtrados <- reos_filtrados %>%
+  mutate(
+    num_delitos = sapply(strsplit(DELITOS, "; "), length), # Columna temporal
+    DELITOS = gsub(" \\(\\d+\\)", "", DELITOS)
+  ) %>%
+  filter(num_delitos > 1) %>%
+  select(-num_delitos) # Eliminamos columna temporal
+
+
+# -------------------- Formato {3,2,1},{0} -> {0},{1} --------------------------
+
+
+all(unique(base_igi_bin$COD_PERS) %in% unique(reos_filtrados$COD_PERS))
+
+# 8731 códigos están en ambas bases de datos, un 40.553 % de las disponibles en base_igi
+# (para el resto de casos, con num_delitos > 1, hay otro 23.385 %)
+cod_pers_baseigi_inter_reinc <- intersect(unique(base_igi_bin$COD_PERS), unique(reos_filtrados$COD_PERS))
+(length(cod_pers_baseigi_inter_reinc) / length(unique(base_igi_bin$COD_PERS))) * 100
+
+reos_filtrados$
+
+
+
+
+
+
 descriptivo_grupal <- c("HD1","HD2","HD3","HD4","HD5","HD7","HD8",#"HD6",
                         "EDU9","EDU10","EDU11","EDU12","EDU13","EDU14","EDU15","EDU16","EDU17",
                         "FAM18","FAM19","FAM20","FAM21",
@@ -28,19 +74,3 @@ descriptivo_grupal <- c("HD1","HD2","HD3","HD4","HD5","HD7","HD8",#"HD6",
                         "CAD28","CAD29","CAD30","CAD31","CAD32","CAD33","CAD34","CAD35",
                         "PRO36","PRO37","PRO38","PRO39",
                         "PAT40","PAT41","PAT42","PAT43")
-
-# -------------------- Formato {3,2,1},{0} -> {0},{1} --------------------------
-
-base_igi_bin <- read.csv("psy_net_files/base_igi_bin_1.csv")
-reinc_por_reo <- readRDS("documentos carceles - ising/Datos reos - sensible/reinc_por_reo.rds")
-
-length(unique(base_igi_bin$COD_PERS))
-length(unique(reinc_por_reo$COD_PERS))
-
-all(unique(base_igi_bin$COD_PERS) %in% unique(reinc_por_reo$COD_PERS))
-
-# 8731 códigos están en ambas bases de datos, un 50.538 % de las disponibles en base_igi
-cod_pers_baseigi_inter_reinc <- intersect(unique(base_igi_bin$COD_PERS), unique(reinc_por_reo$COD_PERS))
-(length(cod_pers_baseigi_inter_reinc) / length(unique(base_igi_bin$COD_PERS))) * 100
-
-reinc_por_reo$
